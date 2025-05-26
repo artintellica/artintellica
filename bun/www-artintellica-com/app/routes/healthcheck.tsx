0c9e1cfa1117f688c37c2187677ef66683ba079a
Ryan X. Charles <ryan@ryanxcharles.com>
2025-05-26 08:57:58 -0700

    healthcheck page

---
+++ bun/www-artintellica-com/app/routes/healthcheck.tsx
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className="mt-4 text-center font-bold text-2xl text-black dark:text-white">
+        OK
+      </h1>
+    </div>
+  );
+}
