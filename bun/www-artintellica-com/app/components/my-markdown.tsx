b7f8972d6502db1cc31ecf8d26772595c00edea6
Ryan X. Charles <ryan@ryanxcharles.com>
2025-05-26 07:21:44 -0700

    add my-markdown

---
+++ bun/www-artintellica-com/app/components/my-markdown.tsx
@@ -0,0 +1,38 @@
+import Markdown from "react-markdown";
+import { Link } from "react-router";
+import smartypants from "remark-smartypants";
+
+export default function MyMarkdown({ children }: { children: string }) {
+  // disable images by replacing "![" with "["
+  children = children.replace(/!\[/g, "[");
+
+  return (
+    <div className="ryanxcharles-prose">
+      <Markdown
+        remarkPlugins={[smartypants]}
+        components={{
+          a: ({ children, href }) => {
+            return (
+              <Link
+                to={
+                  href
+                    ? href.startsWith("./")
+                      ? `.${href}` // force remix to handle relative paths correctly
+                      : href
+                    : href || ""
+                }
+                onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
+                  e.stopPropagation();
+                }}
+              >
+                {children}
+              </Link>
+            );
+          },
+        }}
+      >
+        {children}
+      </Markdown>
+    </div>
+  );
+}
