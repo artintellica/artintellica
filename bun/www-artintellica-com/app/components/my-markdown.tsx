bc48f399e24a96d863ccca58577373ef55560a53
Ryan X. Charles <ryan@ryanxcharles.com>
2025-05-26 13:29:28 -0700

    add gfm

--- bun/www-artintellica-com/app/components/my-markdown.tsx
+++ bun/www-artintellica-com/app/components/my-markdown.tsx
@@ -1,6 +1,7 @@
 import Markdown from "react-markdown";
 import { Link } from "react-router";
 import smartypants from "remark-smartypants";
+import remarkGfm from "remark-gfm";
 
 export default function MyMarkdown({ children }: { children: string }) {
   return (
