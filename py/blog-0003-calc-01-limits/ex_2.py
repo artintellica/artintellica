4cb7e632f049049b3173bcde112c43af217fa3d9
Ryan X. Charles <ryan@ryanxcharles.com>
2025-05-27 12:47:44 -0700

    ex_2

---
+++ py/blog-0003-calc-01-limits/ex_2.py
@@ -0,0 +1,16 @@
+# ex_1.py
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1. sample points around 0
+x = np.linspace(-1e-1, 1e-1, 2001)
+y = np.abs(x)
+
+# 2. plot
+plt.figure(figsize=(5, 4))
+plt.plot(x, y, label=r"$\,\abs x \,$")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title("Zoom on  abs(x)  near 0")
+plt.tight_layout()
+plt.show()
