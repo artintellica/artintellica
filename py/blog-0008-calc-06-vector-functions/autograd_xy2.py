b67cde74f86d778a04f5ebb62affe64abd17c2f8
Ryan X. Charles <ryan@ryanxcharles.com>
2025-05-28 06:07:18 -0700

    format code

--- py/blog-0008-calc-06-vector-functions/autograd_xy2.py
+++ py/blog-0008-calc-06-vector-functions/autograd_xy2.py
@@ -3,13 +3,15 @@ import torch
 
 torch.set_default_dtype(torch.float64)
 
+
 def f(xy):
     x, y = xy
     return x**2 + y**2
+
 
 point = torch.tensor([1.5, -0.8], requires_grad=True)
 val = f(point)
 val.backward()
 
 print(f"f({point.tolist()}) = {val.item():.3f}")
-print("∇f =", point.grad.tolist())   # should be [2*1.5, 2*(-0.8)]
+print("∇f =", point.grad.tolist())  # should be [2*1.5, 2*(-0.8)]
