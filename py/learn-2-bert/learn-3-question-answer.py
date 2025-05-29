ddb9ace5f4f0b1f4dad29b7e29bf8f3c189310e4
Ryan X. Charles <ryan@ryanxcharles.com>
2025-05-29 12:25:14 -0700

    delete learn-1-* and learn-2-*
    these were earlier experiments and no longer conform to the blog
    structure

--- py/learn-2-bert/learn-3-question-answer.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-qa_pipeline = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
-context = "The capital of France is Paris."
-question = "What is the capital of France?"
-result = qa_pipeline(question=question, context=context)
-print(f"Answer: {result['answer']}")
