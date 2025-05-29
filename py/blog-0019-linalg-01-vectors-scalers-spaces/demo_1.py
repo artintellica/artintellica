4952aa508f6848f71579f9bfcab886c74ab03f38
Ryan X. Charles <ryan@ryanxcharles.com>
2025-05-29 07:28:10 -0700

    demo_1.py

---
+++ py/blog-0019-linalg-01-vectors-scalers-spaces/demo_1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+
+# NumPy vector
+v_numpy = np.array([2, 3])
+print("NumPy vector:", v_numpy)
+
+# PyTorch tensor
+v_torch = torch.tensor([2, 3], dtype=torch.float32)
+print("PyTorch vector:", v_torch)
