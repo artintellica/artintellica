c8b6b2bde073ebfc82135466d8311769a3fcebc2
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-06 08:56:30 -0700

    write a normalize function

--- py/blog-0042-deep-numpy-11-arrays/ex_4.py
+++ py/blog-0042-deep-numpy-11-arrays/ex_4.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+def normalize(X):
+    """
+    Normalize the input array X by subtracting the mean and dividing by the standard deviation.
+    
+    Parameters:
+    X (np.ndarray): Input array to normalize.
+    
+    Returns:
+    np.ndarray: Normalized array.
+    """
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    normalized_X = (X - mean) / std
+    return normalized_X
