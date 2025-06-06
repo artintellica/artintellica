0b4aacf61f88f2441a2578f2ebe9ccbdf0a33395
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-06 14:51:11 -0700

    ex_4.py

--- py/blog-0044-deep-numpy-13-mathematical/ex_4.py
+++ py/blog-0044-deep-numpy-13-mathematical/ex_4.py
@@ -4,3 +4,8 @@ from typing import Union
 from neural_network import normalize, matrix_multiply, sigmoid
 
 Z: NDArray[np.floating] = np.array([[1, -1], [2, -2]], dtype=np.float64)
+Z_manual_sigmoid: NDArray[np.floating] = 1 / (1 + np.exp(-Z))
+Z_sigmoid: NDArray[np.floating] = sigmoid(Z)
+print("Input matrix Z (2x2):\n", Z)
+print("Manual sigmoid of Z (1 / (1 + e^(-Z))):\n", Z_manual_sigmoid)
+print("Sigmoid of Z using the sigmoid function:\n", Z_sigmoid)
