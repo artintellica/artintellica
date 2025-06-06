4248245e44b7280cb39688172c455ce94332d96a
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-06 08:51:53 -0700

    ex_2.py

--- py/blog-0042-deep-numpy-11-arrays/ex_2.py
+++ py/blog-0042-deep-numpy-11-arrays/ex_2.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+X = np.array([[1, 2, 3], [4, 5, 6]])
+row_vec = np.array([10, 20, 30])
+added = X + row_vec
+print("X:\n", X)
+print("Row vector:\n", row_vec)
+print("X + row vector:\n", added)
