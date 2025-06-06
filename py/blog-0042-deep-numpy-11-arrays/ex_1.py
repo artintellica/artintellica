b81de0c09a74fa3380c42d1b48f7e113da546bf9
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-06 08:49:50 -0700

    ex_1.py: reshape

--- py/blog-0042-deep-numpy-11-arrays/ex_1.py
+++ py/blog-0042-deep-numpy-11-arrays/ex_1.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+ones = np.ones((4, 3))
+print("Original array of ones:\n", ones)
+print("Shape of ones:", ones.shape)  # (4, 3)
+reshaped = ones.reshape(3, 4)
+print("Reshaped array to 3x4:\n", reshaped)
+print("Shape of reshaped ones:", reshaped.shape)  # (3, 4)
