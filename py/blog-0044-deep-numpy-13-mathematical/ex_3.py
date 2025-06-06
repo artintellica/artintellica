d62864f82e449682cc511507007e7c58ab93ee24
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-06 14:49:13 -0700

    ex_3.py

--- py/blog-0044-deep-numpy-13-mathematical/ex_3.py
+++ py/blog-0044-deep-numpy-13-mathematical/ex_3.py
@@ -4,3 +4,6 @@ from typing import Union
 from neural_network import normalize, matrix_multiply, sigmoid
 
 Z: NDArray[np.floating] = np.array([[-1.5, 2.0], [0.0, -0.5], [3.0, -2.0]], dtype=np.float64)
+Z_operated: NDArray[np.floating] = np.maximum(0, Z)
+print("Input matrix Z (3x2):\n", Z)
+print("ReLU-like operation (max(0, Z)):\n", Z_operated)
