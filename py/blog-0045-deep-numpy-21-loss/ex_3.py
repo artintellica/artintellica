083b292694d7f280b65f9e5722b14a839fc367ff
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-08 12:16:32 -0700

    ex_3.py

---
+++ py/blog-0045-deep-numpy-21-loss/ex_3.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import Union
+from neural_network import normalize, matrix_multiply, mse_loss, sigmoid
+
+X: NDArray[np.floating] = np.array([[1.0], [2.0], [3.0]])
+W: NDArray[np.floating] = np.array([[1.0]])
+b: float = 0.5
+y_pred: NDArray[np.floating] = X @ W + b
+y_true: NDArray[np.floating] = np.array([[2.0], [3.5], [5.0]])
+loss: np.floating = mse_loss(y_true, y_pred)
+print("Input matrix X (3x1):\n", X)
+print("Weights matrix W (1x1):\n", W)
+print("Bias b (scalar):\n", b)
+print("Predicted values y_pred (3x1):\n", y_pred)
+print("True values y_true (3x1):\n", y_true)
+print("Mean Squared Error Loss:", loss)
