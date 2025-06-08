3dbb3039a53a425953e6cab4fe1aac058ba5e5b6
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-08 12:11:10 -0700

    ex_2.py

--- py/blog-0045-deep-numpy-21-loss/ex_2.py
+++ py/blog-0045-deep-numpy-21-loss/ex_2.py
@@ -5,3 +5,9 @@ from neural_network import normalize, matrix_multiply, mse_loss, sigmoid
 
 Z: NDArray[np.floating] = np.array([[1.0], [-1.0], [2.0]])
 A: NDArray[np.floating] = sigmoid(Z)
+y_true: NDArray[np.floating] = np.array([[1.0], [0.0], [1.0]])
+loss: np.floating = mse_loss(A, y_true)
+print("Raw outputs Z (3x1):\n", Z)
+print("Sigmoid of Z (1 / (1 + e^(-Z))):\n", A)
+print("True labels y_true (3x1):\n", y_true)
+print("Mean Squared Error Loss:", loss)
