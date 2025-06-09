a35602ca480d7298f48febc045ec26b8b8d01aaa
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-09 13:58:38 -0700

    ex_2.py

---
+++ py/blog-0055-deep-numpy-43-pooling/ex_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import Union
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from neural_network import conv2d, softmax, normalize, max_pool
+
+
+# Your code here
+X = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+pooled = max_pool(X, size=2, stride=1)
+print("Input Feature Map (4x4):\n", X)
+print("Output after Max Pooling (stride=1):\n", pooled)
+print("Output Shape:", pooled.shape)
