8295921ca739abc0d50100f689a5d61fc618f073
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-09 15:20:40 -0700

    ex_1.py

--- py/blog-0057-deep-numpy-45-advanced/ex_1.py
+++ py/blog-0057-deep-numpy-45-advanced/ex_1.py
@@ -18,4 +18,16 @@ from neural_network import (
 )
 import matplotlib.pyplot as plt
 
-
+gradient1 = np.array([[1.0, 2.0], [3.0, 4.0]])
+gradient2 = np.array([[0.5, 1.5], [2.5, 3.5]])
+velocity = np.zeros((2, 2))
+mu = 0.9
+lr = 0.1
+# First update
+velocity, update = momentum_update(velocity, gradient1, mu, lr)
+print("First Update - Velocity:\n", velocity)
+print("First Update - Parameter Update:\n", update)
+# Second update
+velocity, update = momentum_update(velocity, gradient2, mu, lr)
+print("Second Update - Velocity:\n", velocity)
+print("Second Update - Parameter Update:\n", update)
