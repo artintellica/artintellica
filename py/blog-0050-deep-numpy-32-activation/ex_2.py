a7f62552739081a93f32a7825e204b26e9243a21
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-09 09:26:39 -0700

    ex_2.py

--- py/blog-0050-deep-numpy-32-activation/ex_2.py
+++ py/blog-0050-deep-numpy-32-activation/ex_2.py
@@ -1,4 +1,11 @@
 import numpy as np
 from numpy.typing import NDArray
 from typing import Union
-from neural_network import relu, sigmoid
+from neural_network import relu, sigmoid, softmax
+
+Z = np.random.randn(3, 5)
+A_softmax = softmax(Z)
+sums = np.sum(A_softmax, axis=1)
+print("Input Z (3x5):\n", Z)
+print("Output after Softmax (3x5):\n", A_softmax)
+print("Sum of probabilities per sample (should be ~1):\n", sums)
