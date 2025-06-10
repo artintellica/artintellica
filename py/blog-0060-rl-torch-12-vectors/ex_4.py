30be9b5add0d34fdf7875e75f9dfe4d2634f06f1
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 08:33:51 -0700

    ex_4.py

--- py/blog-0060-rl-torch-12-vectors/ex_4.py
+++ py/blog-0060-rl-torch-12-vectors/ex_4.py
@@ -1,5 +1,14 @@
 import torch
+import matplotlib.pyplot as plt
 
 # Create a PyTorch vector containing $100$ linearly spaced points between $0$ and $4\pi$.
 t: torch.Tensor = torch.linspace(0, 4 * torch.pi, 100)
+t_cos: torch.Tensor = torch.cos(t)
 
+plt.figure(figsize=(8, 4))
+plt.plot(t.numpy(), t_cos.numpy())
+plt.title("Cosine Wave")
+plt.xlabel("t")
+plt.ylabel("cos(t)")
+plt.grid(True)
+plt.show()
