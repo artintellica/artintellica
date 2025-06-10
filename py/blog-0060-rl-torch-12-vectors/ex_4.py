f06a5bdc86ef1a63a70aa67910cf722e87c767b0
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 08:40:41 -0700

    format code

--- py/blog-0060-rl-torch-12-vectors/ex_4.py
+++ py/blog-0060-rl-torch-12-vectors/ex_4.py
@@ -1,7 +1,8 @@
 import torch
 import matplotlib.pyplot as plt
 
-# Create a PyTorch vector containing $100$ linearly spaced points between $0$ and $4\pi$.
+# Create a PyTorch vector containing $100$ linearly spaced points between $0$
+# and $4\pi$.
 t: torch.Tensor = torch.linspace(0, 4 * torch.pi, 100)
 t_cos: torch.Tensor = torch.cos(t)
 
