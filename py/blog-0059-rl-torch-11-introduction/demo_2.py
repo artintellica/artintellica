c4cb02e6f3e04e08af884b5a0af4dae656ed907c
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 05:30:31 -0700

    demo_2.py

---
+++ py/blog-0059-rl-torch-11-introduction/demo_2.py
@@ -0,0 +1,16 @@
+import torch
+
+# Create a 1D tensor of floats, from 0 to 4
+x: torch.Tensor = torch.arange(5, dtype=torch.float32)
+
+# Detect GPUs or MPS device (Apple Silicon)
+if torch.cuda.is_available():
+    device: torch.device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print(f"Using device: {device}")
+
+x_gpu: torch.Tensor = x.to(device)
+print("x is on device:", x_gpu.device)
