3dfcedca1c27557f4a49e90720acda53837804c4
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 08:22:14 -0700

    fix ex_1.py

--- py/blog-0060-rl-torch-12-vectors/ex_1.py
+++ py/blog-0060-rl-torch-12-vectors/ex_1.py
@@ -7,3 +7,10 @@ print("t as Python float:", t_py)  # Convert to Python float
 
 u: torch.Tensor = torch.tensor([3, 1, 4, 1, 5, 9], dtype=torch.float32)
 print("u:", u)  # Tensor data
+
+u_col: torch.Tensor = u.unsqueeze(1)  # Reshape to a column vector (n, 1)
+print("u reshaped to 2D (shape):", u_col.shape)
+print("u reshaped to 2D (data):", u_col)  # Reshaped tensor data
+u_back: torch.Tensor = u_col.squeeze()  # Remove the added dimension
+print("u reshaped back to 1D (shape):", u_back.shape)
+print("u reshaped back to 1D (data):", u_back)  # Reshaped back to 1D tensor data
