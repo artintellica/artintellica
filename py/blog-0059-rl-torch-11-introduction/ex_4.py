37af4f39e1623b1462c70e88879820e09ec7df09
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 05:41:06 -0700

    simplify ex_4.py

--- py/blog-0059-rl-torch-11-introduction/ex_4.py
+++ py/blog-0059-rl-torch-11-introduction/ex_4.py
@@ -2,9 +2,6 @@ import torch
 
 print("PyTorch version:", torch.__version__)
 
-a: torch.Tensor = torch.tensor([1, 2, 3], dtype=torch.float32)
-print("a:", a)  # Tensor data
-
 if torch.cuda.is_available():
     device: torch.device = torch.device("cuda")
 elif torch.backends.mps.is_available():
@@ -12,10 +9,6 @@ elif torch.backends.mps.is_available():
 else:
     device = torch.device("cpu")
 print(f"Using device: {device}")
-
-a_gpu: torch.Tensor = a.to(device)
-print("a is on device:", a_gpu.device)
-print("a_gpu:", a_gpu)
 
 x: torch.Tensor = torch.ones(6, dtype=torch.float32, device=device)
 y: torch.Tensor = torch.zeros(6, dtype=torch.float32, device=device)
