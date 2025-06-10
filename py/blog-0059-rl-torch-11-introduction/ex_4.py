d0d895be949b7ada2f805f7082da81b5278c4015
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 05:40:12 -0700

    ex_4.py

---
+++ py/blog-0059-rl-torch-11-introduction/ex_4.py
@@ -0,0 +1,23 @@
+import torch
+
+print("PyTorch version:", torch.__version__)
+
+a: torch.Tensor = torch.tensor([1, 2, 3], dtype=torch.float32)
+print("a:", a)  # Tensor data
+
+if torch.cuda.is_available():
+    device: torch.device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print(f"Using device: {device}")
+
+a_gpu: torch.Tensor = a.to(device)
+print("a is on device:", a_gpu.device)
+print("a_gpu:", a_gpu)
+
+x: torch.Tensor = torch.ones(6, dtype=torch.float32, device=device)
+y: torch.Tensor = torch.zeros(6, dtype=torch.float32, device=device)
+z: torch.Tensor = x + y  # elementwise add
+print("z:", z)
