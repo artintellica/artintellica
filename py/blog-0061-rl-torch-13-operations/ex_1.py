4f213bd8bb128c53bb6cc920f2a592246a25138f
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 08:55:04 -0700

    ex_1.py

--- py/blog-0061-rl-torch-13-operations/ex_1.py
+++ py/blog-0061-rl-torch-13-operations/ex_1.py
@@ -1,1 +1,8 @@
 import torch
+
+a: torch.Tensor = torch.tensor([2, 3, 4, 5], dtype=torch.float32)
+b: torch.Tensor = torch.tensor([1, 1, 1, 1], dtype=torch.float32)
+sum_ab: torch.Tensor = a + b
+print("a:", a)
+print("b:", b)
+print("a + b:", sum_ab)
