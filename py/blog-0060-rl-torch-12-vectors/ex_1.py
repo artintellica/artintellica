db540b473ff696173d3e2608d1cefb1397c4710c
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 08:17:16 -0700

    next step in ex_1.py

--- py/blog-0060-rl-torch-12-vectors/ex_1.py
+++ py/blog-0060-rl-torch-12-vectors/ex_1.py
@@ -4,3 +4,6 @@ t: torch.Tensor = torch.tensor(42, dtype=torch.float32)
 print("t:", t)  # Tensor data
 t_py: float = t.item()
 print("t as Python float:", t_py)  # Convert to Python float
+
+u: torch.Tensor = torch.tensor([3, 1, 4, 1, 5, 9], dtype=torch.float32)
+print("u:", u)  # Tensor data
