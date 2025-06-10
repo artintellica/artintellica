6b9e25aa8dd025c2dfc7318d4b9948967e074659
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 09:46:47 -0700

    finish ex_1.py

--- py/blog-0062-rl-torch-14-matrices/ex_1.py
+++ py/blog-0062-rl-torch-14-matrices/ex_1.py
@@ -4,3 +4,8 @@ a: torch.Tensor = torch.arange(0, 12, dtype=torch.float32)
 print("a:", a)
 b: torch.Tensor = a.reshape(3, 4)
 print("b:", b)
+
+c: torch.Tensor = torch.rand(2 * 5, dtype=torch.float32)
+print("c:", c)
+d: torch.Tensor = c.reshape(2, 5)
+print("d:", d)
