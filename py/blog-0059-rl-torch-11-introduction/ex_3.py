1db0804ed86c0594892e9113b6b084a73a073d1a
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-10 05:37:20 -0700

    ex_3.py

--- py/blog-0059-rl-torch-11-introduction/ex_3.py
+++ py/blog-0059-rl-torch-11-introduction/ex_3.py
@@ -12,3 +12,7 @@ elif torch.backends.mps.is_available():
 else:
     device = torch.device("cpu")
 print(f"Using device: {device}")
+
+a_gpu: torch.Tensor = a.to(device)
+print("a is on device:", a_gpu.device)
+print("a_gpu:", a_gpu)
