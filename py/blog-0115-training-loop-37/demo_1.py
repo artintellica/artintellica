18fe5485764c12721e5628b241a4ae19c1f81c0a
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-18 10:56:16 -0700

    init demo_1.py

---
+++ py/blog-0115-training-loop-37/demo_1.py
@@ -0,0 +1,13 @@
+
+import torch
+
+def get_device() -> torch.device:
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
+
+device = get_device()
+print(f"Using device: {device}")
