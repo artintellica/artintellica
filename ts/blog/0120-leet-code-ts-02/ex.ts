6ac94ce6a8b2fabf3b55a9f7c45413085a2171df
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-23 12:36:03 -0700

    init ex.ts

---
+++ ts/blog/0120-leet-code-ts-02/ex.ts
@@ -0,0 +1,21 @@
+class ListNode {
+  val: number;
+  next?: ListNode;
+
+  constructor(val: number, next?: ListNode) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+function createList(n: number): ListNode {
+  if (n > 1) {
+    const node = new ListNode(n);
+    node.next = createList(n - 1);
+    return node;
+  }
+  const node = new ListNode(n);
+  return node;
+}
+
+const list = createList(5);
