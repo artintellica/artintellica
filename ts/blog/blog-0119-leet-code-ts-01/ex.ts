c7e412222331bd15e9f9eef6b60231326cce8428
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-23 10:07:42 -0700

    ex.ts

---
+++ ts/blog/blog-0119-leet-code-ts-01/ex.ts
@@ -0,0 +1,23 @@
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+function twoSum(
+  nums: number[],
+  target: number,
+): { numIdx: number; complementIdx: number } | null {
+  const map: Map<number, number> = new Map();
+  for (let idx = 0; idx < nums.length; idx++) {
+    const num = nums[idx];
+    const complement = target - num;
+    const complementIdx = map.get(complement);
+    if (complementIdx !== undefined) {
+      const numIdx = idx;
+      return { numIdx, complementIdx };
+    }
+    map.set(num, idx);
+  }
+  return null; // no solution found
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // solution
+console.log(twoSum([2, 7, 11, 15], 200)); // no solution
