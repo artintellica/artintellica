b3ed2551ec8f6943b1fee4f61648364a46063ef2
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-23 15:53:14 -0700

    ex2.ts

---
+++ ts/blog/0123-leet-code-ts-15/ex2.ts
@@ -0,0 +1,22 @@
+function maxProfit2(prices: number[]): number {
+  if (prices.length < 2) {
+    return 0;
+  }
+
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    const price = prices[i] as number;
+    const yesterdayPrice = prices[i - 1] as number;
+    if (price > yesterdayPrice) {
+      profit += price - yesterdayPrice;
+    }
+  }
+
+  return profit;
+}
+
+console.log(maxProfit2([7, 1, 5, 3, 6, 4])); // Output: 5 (buy at 1, sell at 6)
+console.log(maxProfit2([7, 6, 4, 3, 1])); // Output: 0 (no profit possible)
+console.log(maxProfit2([1, 2])); // Output: 1 (buy at 1, sell at 2)
+console.log(maxProfit2([2])); // Output: 0 (can't sell)
