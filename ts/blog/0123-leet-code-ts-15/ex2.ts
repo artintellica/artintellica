a0290b2d9c8462349994d38def276c321a424306
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-24 05:01:12 -0700

    fix output

--- ts/blog/0123-leet-code-ts-15/ex2.ts
+++ ts/blog/0123-leet-code-ts-15/ex2.ts
@@ -16,7 +16,6 @@ function maxProfit2(prices: number[]): number {
   return profit;
 }
 
-console.log(maxProfit2([7, 1, 5, 3, 6, 4])); // Output: 5 (buy at 1, sell at 6)
+console.log(maxProfit2([7, 1, 5, 3, 6, 4])); // Output: 7
+console.log(maxProfit2([1, 2, 3, 4, 5])); // Output: 4 (profit on each consecutive day)
 console.log(maxProfit2([7, 6, 4, 3, 1])); // Output: 0 (no profit possible)
-console.log(maxProfit2([1, 2])); // Output: 1 (buy at 1, sell at 2)
-console.log(maxProfit2([2])); // Output: 0 (can't sell)
